db23e052add8de482a79e9bd38f7fcab7b3270db
blendaabazi <ba64884@ubt-uni.net>
2025-04-18 01:25:13 +0200

    ChatBox in FrontEnd

--- client/src/components/chat/chat.jsx
+++ client/src/components/chat/chat.jsx
@@ -4,63 +4,54 @@ import "./chat.scss";
 function Chat() {
   const [chat, setChat] = useState(null);
 
+  const handleSelect = (user) => {
+    setChat(user); // user mund të jetë emri apo objekti i përdoruesit
+  };
+
   return (
     <div className="chat">
       <div className="messages">
         <h1>Messages</h1>
-        <div className="message">
-        <img src="/noavatar.jpg" alt="" />
-        <span>Joe</span>
-        <p>.....mesage</p>
-        </div>
-        <div className="message">
-        <img src="/noavatar.jpg" alt="" />
-        <span>Joe</span>
-        <p>.....mesage</p>
-        </div>
-        <div className="message">
-        <img src="/noavatar.jpg" alt="" />
-        <span>Joe</span>
-        <p>.....mesage</p>
-        </div>
-        <div className="message">
-        <img src="/noavatar.jpg" alt="" />
-        <span>Joe</span>
-        <p>.....mesage</p>
-        </div>
+
+        {["Blenda", "Altin", "Vlerona", "Dren"].map((user, index) => (
+          <div className="message" key={index} onClick={() => handleSelect(user)}>
+            <img src="/noavatar.jpg" alt="" />
+            <span>{user}</span>
+          </div>
+        ))}
       </div>
 
-      {<div className="chatBox">
-        <div className="top">
-          <div className="user">
-            <img src="/logo.png" alt="" />
-            john
+      {chat && (
+        <div className="chatBox">
+          <div className="top">
+            <div className="user">
+              <img src="/logo.png" alt="" />
+              {chat}
+            </div>
+            <span className="close" onClick={() => setChat(null)}>x</span>
           </div>
-          <span className="close">x</span>
+
+          <div className="center">
+            <div className="chatMessage own">
+              <p>Hello {chat}, how are you?</p>
+              <span>2 mins ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>I’m good, thank you! How about you?</p>
+              <span>1 min ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Just working on the new project.</p>
+              <span>Just now</span>
+            </div>
+          </div>
+
+          <div className="bottom">
+            <textarea placeholder="Type your message..." />
+            <button>Send</button>
+          </div>
         </div>
-        <div className="center">
-          <div className="chatMessage own">
-            <p>message here</p>
-            <span>1 hour</span>
-          </div>
-          <div className="chatMessage">
-            <p>message here</p>
-            <span>1 hour</span>
-          </div>
-          <div className="chatMessage">
-            <p>message here</p>
-            <span>1 hour</span>
-          </div>
-          <div className="chatMessage own">
-            <p>message here</p>
-            <span>1 hour</span>
-          </div>
-        </div>
-        <div className="bottom">
-          <textarea name="" id=""></textarea>
-          <button>Send</button>
-        </div>
-      </div>}
+      )}
     </div>
   );
 }
